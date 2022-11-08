eec5ef5e995c82cb4c2ab2d53c8ac17fa50be6af
Nguyễn Thị Vĩnh Uyên <vinhuyen.theonly@gmail.com>
2022-11-08 16:27:02 +0700

    Create Login.js

---
+++ screens/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const Login = () => {
+  return (
+    <SafeAreaView>
+      <Text>ejfrjf</Text>
+    </SafeAreaView>
+  );
+};
+export default Login;
